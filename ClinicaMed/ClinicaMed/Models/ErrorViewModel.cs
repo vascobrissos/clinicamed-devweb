ae9c16504dfa3685236602e2bcc1d41a09c2339a
owanedee <claudiojorge9090@gmail.com>
2024-05-03 17:17:01 +0100

    Criação do Projeto

---
+++ ClinicaMed/ClinicaMed/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClinicaMed.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
