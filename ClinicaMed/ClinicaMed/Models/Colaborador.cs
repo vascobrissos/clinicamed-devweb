bc4dda73d7a48baf6db9fabd81a96baa8247c8d6
owanedee <claudiojorge9090@gmail.com>
2024-05-04 15:47:58 +0100

    Criacao das Classes Colaborador e Receita

---
+++ ClinicaMed/ClinicaMed/Models/Colaborador.cs
@@ -0,0 +1,119 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicaMed.Models
+{
+    public class Colaborador
+    {
+        public Colaborador()
+        {
+            ListaReceita = new HashSet<Receita>();
+        }
+
+        /// <summary>
+        /// Identificador único de um colaborador, chave primária, inteiro tamanho max 10
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Username do colaborador para login, varchar(255)
+        /// </summary>
+        [StringLength(255)]
+        public string UsernameColaborador { get; set; }
+
+        /// <summary>
+        /// Nome de apresentação do colaborador, varchar(255) 
+        /// </summary>
+        [StringLength(255)]
+        public string NomeApresentacao { get; set; }
+
+        /// <summary>
+        /// Password do colaborador, varchar(255)
+        /// </summary>
+        [StringLength(255)]
+        public string PwdColaborador { get; set; }
+
+        /// <summary>
+        /// Nome do colaborador, varchar(100)
+        /// </summary>
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Apelido do colaborador, varchar(100)
+        /// </summary>
+        [StringLength(100)]
+        public string Apelido { get; set; }
+
+
+        /// <summary>
+        /// Telemovel do utilizador
+        /// </summary>
+        [Display(Name = "Telemóvel")]
+        [StringLength(9)]
+        [RegularExpression("9[1236][0-9]{7}", ErrorMessage = "o {0} introduzido é inválido")] // o {0} referencia o "Telemóvel"
+        public string Telemovel { get; set; }
+
+        /// <summary>
+        /// Email do colaborador
+        /// </summary>
+        [StringLength(100)]
+        public string Email{ get; set; }
+
+        /// <summary>
+        /// Sexo/Genero do Colaborador, por analisar, indeciso se usamos int ou string
+        /// </summary>
+        public int Sexo { get; set; }
+
+        /// <summary>
+        /// Data de nascimento do colaborador, tipo DateOnly
+        /// </summary>
+        public DateOnly DataNascimento { get; set; }
+
+
+        /// <summary>
+        /// Pais do Colaborador
+        /// </summary>
+        [StringLength (100)]
+        public string Pais { get; set; }
+
+        /// <summary>
+        /// Morada do Colaborador
+        /// </summary>
+        [StringLength(100)]
+        public string morada { get; set; }
+
+        /// <summary>
+        /// Codigo postal do colaborador
+        /// </summary>
+        [StringLength(100)]
+        public string CodPostal { get; set; }
+
+        /// <summary>
+        /// Localidade do colaborador
+        /// </summary>
+        [StringLength(100)]
+        public string Localidade { get; set; }
+
+        /// <summary>
+        /// Numero de Identificação Fiscal do colaborado, deve possuir 9 digitos de 0 a 9 
+        /// </summary>
+        [StringLength(9)]
+        [RegularExpression("[0-9]{9}", ErrorMessage = "o {0} introduzido é inválido")]
+        public string Nif { get; set; }
+
+        /// <summary>
+        /// Estado civil do colaborador, indeciso se deve ser int ou string
+        /// </summary>
+        public int Estado { get; set; }
+
+        /// <summary>
+        /// Identifica o numero de ordem do colaborador
+        /// </summary>
+        public int NumOrdem { get; set; }
+
+
+        //Coleção que referencia a relação de tipo 1-N com a class Receita
+        public ICollection<Receita> ListaReceita { get; set; }
+    }
+}
---
+++ ClinicaMed/ClinicaMed/Models/Receita.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicaMed.Models
+{
+    public class Receita
+    {
+        /// <summary>
+        /// Identificador único da receita, chave primária
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Numero da receita?
+        /// </summary>
+        [StringLength(150)]
+        public string NumReceita { get; set; }
+
+        /// <summary>
+        /// Notas/Observações sobre a receita
+        /// </summary>
+        [StringLength(1000)]
+        public string Notas { get; set; }
+
+        /// <summary>
+        /// Data e hora da receita
+        /// </summary>
+        public DateTime DataReceita { get; set; }
+
+        /// <summary>
+        /// Identificador do Processo referente à receita
+        /// </summary>
+        // Deve ser uma chave estrangeira com a class Processo
+        public int IdProcesso { get; set; }
+
+        /// <summary>
+        /// Identificador do Colaborador referente à receita
+        /// </summary>
+        // Deve ser uma chave estrangeira com a class Colaborador
+        [ForeignKey(nameof(Colaborador))]
+        public int ColaboradorFK { get; set; }
+        public Colaborador Colaborador { get; set; }
+
+        /// <summary>
+        /// Identifica o estado da receita. REVER CONCEITO
+        /// </summary>
+        public int Estado { get; set; }
+    }
+}
