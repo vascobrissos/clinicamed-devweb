1470b5023b362e8184c7e0df37c590fb8b44823f
Vasco Alves <vascobrissos@gmail.com>
2024-06-26 16:48:43 +0100

    Merge Conflict DB Resolved

---
+++ ClinicaMed/ClinicaMed/Models/Consulta.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicaMed.Models
+{
+    public class Consulta
+    {
+        /// <summary>
+        /// Identificado unico da consulta
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Data da consulta
+        /// </summary>
+        public DateTime DataConsulta { get; set; }
+
+        /// <summary>
+        /// Observacoes uteis pro parte do colaborador sobre a consulta
+        /// </summary>
+        [StringLength(100)]
+        public string Observacoes { get; set; }
+
+        /// <summary>
+        /// Estado da consulta
+        /// </summary>
+        public int Estado { get; set; }
+
+        //******************************************************//
+        // Chaves forasteiras das tabelas Colaborador e Processo//
+        //******************************************************//
+
+        [ForeignKey(nameof(Colaborador))]
+        public int ColaboradorFK { get; set; }
+        public Colaborador Colaborador { get; set; }
+
+        [ForeignKey(nameof(Processo))]
+        public int ProcessoFK { get; set; }
+        public Colaborador Processo { get; set; }
+    }
+}
